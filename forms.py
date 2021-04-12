88e0a3a17e81f02f9815a53b34d08e49eb74e3a2
farhad <alireza.rezazadeh.official@gmail.com>
2021-04-13 01:03:35 +0430

    polishing register and login section

--- forms.py
+++ forms.py
@@ -5,24 +5,17 @@ from markupsafe import Markup
 
 
 class RegistrationForm(FlaskForm):
-    username_markup = Markup('<i class="zmdi zmdi-account material-icons-name"></i>')
-    email_markup = Markup('<i class="zmdi zmdi-email"></i>')
-    password_markup = Markup('<i class="zmdi zmdi-lock"></i>')
-    confirm_password_markup = Markup('<i class="zmdi zmdi-lock-outline"></i>')
-
-    username = StringField(username_markup, validators=[DataRequired(), Length(min=2, max=20)])
-    email = StringField(email_markup, validators=[DataRequired(), Email()])
-    password = PasswordField(password_markup, validators=[DataRequired()])
-    confirm_password = PasswordField(confirm_password_markup, validators=[DataRequired(), EqualTo("password")])
+    username = StringField(validators=[DataRequired(), Length(min=2, max=20)])
+    email = StringField(validators=[DataRequired(), Email()])
+    password = PasswordField(validators=[DataRequired()])
+    confirm_password = PasswordField(validators=[DataRequired(), EqualTo("password")])
     submit = SubmitField("Sign Up")
 
 
 class LoginForm(FlaskForm):
-    email_markup = Markup('<i class="zmdi zmdi-email"></i>')
-    password_markup = Markup('<i class="zmdi zmdi-lock"></i>')
-    submit_markup = Markup("<span><span></span></span>Remember me")
+    remember_markup = Markup('<span class="caption">Remember me</span>')
 
-    email = StringField(email_markup, validators=[DataRequired(), Email()])
-    password = PasswordField(password_markup, validators=[DataRequired()])
-    remember = BooleanField(submit_markup)
+    email = StringField(validators=[DataRequired(), Email()])
+    password = PasswordField(validators=[DataRequired()])
+    remember = BooleanField("Remember me")
     submit = SubmitField("Log In")
